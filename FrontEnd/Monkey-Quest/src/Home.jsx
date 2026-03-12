c3764e228d5d4c0e7be82ce3da781c263879f06f
isum-05 <isumh2005@gmail.com>
2026-03-12 22:40:33 +0530

    ui updated

--- FrontEnd/Monkey-Quest/src/Home.jsx
+++ FrontEnd/Monkey-Quest/src/Home.jsx
@@ -1,23 +1,58 @@
-function Home(){
+import {useEffect,useState} from "react";
+
+function Home({setPage,user}){
+    const [joke, setJoke] = useState("Thinking ....");
+
+    useEffect(()=>{
+        monkeyJoke();
+        const timer = setInterval(monkeyJoke,5000);
+
+        return ()=> clearInterval(timer)
+    },[]);
+
+
+    function monkeyJoke(){
+        fetch("https://icanhazdadjoke.com/", {
+            headers: { "Accept": "application/json" }
+        })
+        .then(res => res.json())
+        .then(data => {
+            setJoke(data.joke);
+        });
+    }
+
+    function checkPlayer(){
+        const savedUser = localStorage.getItem("username");
+        const session = sessionStorage.getItem("loggedIn");
+
+        if(savedUser && session){
+        setPage("game");  
+        }
+        else{
+        setPage("login");  
+        }
+
+    }
+
     return(
-        <>
-        <div class="container">
-            <div class="left-section">
-                <div class="logo-image">
-                    <img src="img/logo2.png" alt="MonkeyQuest Logo" />
+        <div className="container">
+            <div className="left-section">
+                <div className="logo-image">
+                    <img src="src/assets/logo2.png" alt="MonkeyQuest game logo featuring a monkey character in a vibrant jungle setting" />
                 </div>
-                <div class="popup-section"></div>
+                <div className="popup-section"></div>
             </div>
-            <div class="right-section">
-                <div class="Monkey-message-section">
-                    <div class="text-box" id="monkeyText"></div>
+            <div className="right-section">
+                <div className="Monkey-message-section">
+                    <div className="text-box" >
+                        {joke}
+                    </div>
                 </div>
-                <div class="play-dashboard">
-                    <button class="play"><img src="img/playImg.png" alt="play" /></button>
+                <div className="play-dashboard">
+                    <button className="play" onClick={checkPlayer}><img src="/src/assets/playImg.png" alt="Play button to start the MonkeyQuest maze game" /></button>
                 </div>
             </div>
         </div>
-        </>
     );
 }
 
